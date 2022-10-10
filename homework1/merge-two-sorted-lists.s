979a459446487e24bdfc7b8406d5ac3529f17bc0
ubuntu-chenzenyang <chenzenyang0905@gmail.com>
2022-10-10 18:30:22 +0800

    Delete space line

--- homework1/merge-two-sorted-lists.s
+++ homework1/merge-two-sorted-lists.s
@@ -5,14 +5,12 @@ list2:
     .word 2, 0
 list3:
     .word 4, 0
-    
 list4:
     .word 1, 0
 list5:
     .word 3, 0
 list6:
     .word 4, 0
-    
 strLink:
     .string " -> "
 strNull:
@@ -28,7 +26,6 @@ setLinked:
     sw    x0,  4(t0)
     lw    s2,  0(t0)     # for checking
     lw    s3,  4(t0)     # for checking
-    
     la    t1, list5
     sw    t1, -4(t1)
     addi  t1, t1, 8
@@ -36,12 +33,9 @@ setLinked:
     sw    x0,  4(t1)
     lw    s4,  0(t1)     # for checking
     lw    s5,  4(t1)     # for checking
-    
     la    a0, list1     # a0 = &llist
     la    a1, list4     # a1 = &rlist
     li    a2, 6
-    
-    
 mergeTwoSortedLists:
     addi  s0, sp, -4    # list, sorted, indir
     sw    x0, 0(s0)     # *list = NULL
@@ -49,11 +43,9 @@ mergeTwoSortedLists:
     sw    x0, -8(s0)    # **indir = NULL
     sw    a0, -12(s0)
     sw    a1, -16(s0)
-    
 loop:
     beq   a0, x0, out
     beq   a1, x0, out
-    
     lw    s6, 0(a0)
     lw    s7, 0(a1)
     addi  s8, s0, -12
@@ -62,46 +54,34 @@ loop:
     beq   t2, x0, lSoE
     addi  s9, s0, -16
     sw    s9, -8(s0)    # indir = &rlist
-    
 lSoE:
     lw    t4, -8(s0)
     lw    s11, -4(s0)   # s11 = sorted
     lw    s10, 0(t4)   # s10 = *indir
     sw    s10, 0(s11)   # *sorted = *indir
-    
     addi  t3, s10, 4
     sw    t3, -4(s0)    # sorted = &(*sorted)->next
-
-
-
     lw    t5, 4(s10)
     sw    t5, 0(t4)
-    
     lw    a0, -12(s0)
     lw    a1, -16(s0)
-
     j loop
-    
 out:
     lw    t6, -4(s0)
     or    s1, a0, a1
     sw    s1, 0(t6)
-    
     lw    t0, 0(s0)
 printResult:
     beq   a2, x0, end
     lw    a0, 0(t0)
     li    a7, 1
     ecall
-    
     la    a0, strLink
     li    a7, 4
     ecall
-    
     addi  a2, a2, -1
     lw    t0, 4(t0)
     j printResult
-
 end:
     la    a0, strNull
     li    a7, 4
